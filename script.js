8a64d75ddc5e68162179b92af6a11e6b82f22027
guilherme5V <guiaragao2007@gmail.com>
2024-09-09 16:23:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
 window.onload = function () {
-    document.getElementById("pergunta").textContent =
-        "Hollow knights?";
-    resposta.focus();
-  };
-  const msg = 'Sua localização é ';
-  let timezone; // Variável para armazenar o fuso horário
-  
-  function showUserTime() {
-      if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-          document.getElementById("showUser").innerHTML = "Geolocalização não suportada por este navegador.";
-      }
+  document.getElementById("pergunta").textContent = "Hollow knights?";
+  resposta.focus();
+};
+const msg = "Sua localização é ";
+let timezone; // Variável para armazenar o fuso horário
+
+function showUserTime() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    document.getElementById("showUser").innerHTML =
+      "Geolocalização não suportada por este navegador.";
   }
-  
-  function showPosition(position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      TimezoneJS.timezone.zoneFileVersion = "2019a";
-      TimezoneJS.timezone.init({ async: false });
-      timezone = TimezoneJS.gettimezone({ lat: latitude, lng: longitude }); // Armazena o fuso horário
-  
-      updateTime();
-  }
-  
-  function updateTime() {
-      const time = new Date().toLocaleString("pt-BR", { timeZone: timezone });
-      document.getElementById("showUser").innerHTML = time;
-  }
-  
-  // Chamar a função inicial para obter a localização e exibir a hora
-  showUserTime();
-  
-  // Atualizar a hora a cada 15 segundos
-  setInterval(updateTime, 1000);
-  
-  function atualizarHora() {
-    
-    const agora = new Date();
-    const horas = agora.getHours();
-    const minutos = agora.getMinutes();
-    const segundos = agora.getSeconds();
-    document.getElementById('hora').textContent = `${horas}:${minutos}:${segundos}`;
-  }
-  
-  
+}
+
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  TimezoneJS.timezone.zoneFileVersion = "2019a";
+  TimezoneJS.timezone.init({ async: false });
+  timezone = TimezoneJS.gettimezone({ lat: latitude, lng: longitude }); // Armazena o fuso horário
+
+  updateTime();
+}
+
+function updateTime() {
+  const time = new Date().toLocaleString("pt-BR", { timeZone: timezone });
+  document.getElementById("showUser").innerHTML = time;
+}
+
+// Chamar a função inicial para obter a localização e exibir a hora
+showUserTime();
+
+// Atualizar a hora a cada 15 segundos
+setInterval(updateTime, 1000);
+
+function atualizarHora() {
+  const agora = new Date();
+  const horas = agora.getHours();
+  const minutos = agora.getMinutes();
+  const segundos = agora.getSeconds();
+  document.getElementById(
+    "hora"
+  ).textContent = `${horas}:${minutos}:${segundos}`;
+}
